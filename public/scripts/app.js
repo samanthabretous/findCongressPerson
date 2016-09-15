e71a493e1747f432c54498570d028d735ab84db4
Samantha Bretous <sam@samanthabretous.com>
2016-09-15 14:18:38 -0400

    set up main app

---
+++ public/scripts/app.js
@@ -0,0 +1,35 @@
+// require react and react dom 
+// var React = require('react');
+// var ReactDOM = require('react-dom');
+
+import Form from "./form.js"
+
+// create variable that holds out main class
+var App = React.createClass({
+  render() {
+    return (
+      <div>
+        <Form />
+      </div>
+    )
+  }
+});
+
+
+//we need a google api
+  // concat the users input
+  // make the ajax request
+      //url
+      // success function to find the latitude and long
+// make a second ajax call 
+  // concat long and lat in with congress api
+  // with the data change the data
+
+//address
+//make form input box, button
+
+
+// render to the dom
+ReactDOM.render(<App />, document.getElementById('root'))
+
+
